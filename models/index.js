0e33408f8fefbd83559b409975584c40659e7859
MegMathis <mcdonough.megan25@gmail.com>
2023-04-20 20:22:39 -0400

    models completed

---
+++ models/index.js
@@ -0,0 +1,19 @@
+const User = require("./User");
+const Post = require("./Post");
+const Comment = require("./Comment");
+
+User.hasMany(Post, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+});
+
+Comment.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
+
+module.exports = { User, Post, Comment };
--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers");
+const helpers = require("./utils/helpers");
 
 const sequelize = require("./config/connection");
-const { Sequelize } = require("sequelize");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
