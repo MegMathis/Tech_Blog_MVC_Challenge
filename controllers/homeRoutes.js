097f59b5f802dc45a8ebe7251656d433079b254d
MegMathis <mcdonough.megan25@gmail.com>
2023-04-22 15:22:09 -0400

    homeRoutes working maybe

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -31,26 +31,46 @@ router.get("/", (req, res) => {
 });
 
 // one post
-router.get("/post/:id", async (req, res) => {
-  try {
-    const postData = await Post.findByPk(req.params.id, {
-      include: [
-        {
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "user_id", "post_id"],
+        include: {
           model: User,
           attributes: ["username"],
         },
-      ],
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((postData) => {
+      if (postData) {
+        res.status(404).json({ message: "Post not found with that id" });
+        return;
+      }
+      const post = postData.get({ plain: true });
+      res.render("one-post", { post, logged_in: req.session.logged_in });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
+});
 
-    const post = postData.get({ plain: true });
-
-    res.render("post", {
-      ...post,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
   }
+  res.render("login");
 });
 
 module.exports = router;
