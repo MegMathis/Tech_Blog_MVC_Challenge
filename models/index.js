cf6e0518ad52b49079e9f9a05561c6af48539c39
MegMathis <mcdonough.megan25@gmail.com>
2023-04-23 18:58:04 -0400

    updated user model

--- models/index.js
+++ models/index.js
@@ -2,28 +2,35 @@ const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
 
+// a single user can have many posts
 User.hasMany(Post, {
   foreignKey: "user_id",
   onDelete: "CASCADE",
 });
 
+// Posts can belong to a User
 Post.belongsTo(User, {
   foreignKey: "user_id",
+  onDelete: "SET NULL",
 });
 
+// User can have a comment
 Comment.belongsTo(User, {
   foreignKey: "user_id",
   onDelete: "SET NULL",
 });
 
+// a post can have comments
 Comment.belongsTo(Post, {
   foreignKey: "post_id",
 });
 
+// a single user can have many comments
 User.hasMany(Comment, {
   foreignKey: "user_id",
 });
 
+// a single post can have many comments
 Post.hasMany(Comment, {
   foreignKey: "post_id",
 });
