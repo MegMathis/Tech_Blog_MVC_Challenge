3c15b069474a1cf103b4b5ad29594d83a6416c3a
MegMathis <mcdonough.megan25@gmail.com>
2023-04-25 19:12:04 -0400

    help with tutor with login js

--- public/js/login.js
+++ public/js/login.js
@@ -1,47 +1,76 @@
-async function loginFormHandler(event) {
+const loginFormHandler = async (event) => {
   event.preventDefault();
 
   //  stuff from login form
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
-  if (email && password) {
-    const response = await fetch("/api/users/login", {
-      method: "POST",
-      body: JSON.stringify({ email, password }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    // if works, redirect to profile page
-    if (response.ok) {
+  fetch("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      username: email,
+      password: password,
+    }),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then(function () {
       document.location.replace("/dashboard/");
-    } else {
-      alert(response.statusText);
-    }
-  }
-}
+    })
+    .catch((err) => console.log(err));
+
+  // if (email && password) {
+  //   const response = await fetch("/api/users/login", {
+  //     method: "POST",
+  //     body: JSON.stringify({ email, password }),
+  //     headers: { "Content-Type": "application/json" },
+  //   });
+
+  //   // if works, redirect to profile page
+  //   if (response.ok) {
+  //     document.location.replace("/dashboard/");
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
+};
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
-};
-const username = document.querySelector("#username-signup").value.trim();
-const email = document.querySelector("#email-signup").value.trim();
-const password = document.querySelector("#password-signup").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-if (username && email && password) {
-  const response = await fetch("/api/users", {
+  fetch("/api/user", {
     method: "POST",
-    body: JSON.stringify({ username, email, password }),
-    headers: { "Content-Type": "application/json" },
-  });
+    body: JSON.stringify({
+      username: username,
+      email: email,
+      password: password,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then(function () {
+      document.location.replace("/dashboard/");
+    })
+    .catch((err) => console.log(err));
 
-  // check response status
-  if (response.ok) {
-    document.location.replace("/dashboard/");
-  } else {
-    alert(response.statusText);
-  }
-}
+  // if (username && email && password) {
+  //   const response = await fetch("../../controllers/userRoutes", {
+  //     method: "POST",
+  //     body: JSON.stringify({ username, email, password }),
+  //     headers: { "Content-Type": "application/json" },
+  //   });
+
+  //   // check response status
+  //   if (response.ok) {
+  //     document.location.replace("/dashboard/");
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
+};
 
 document
   .querySelector(".login-form")
